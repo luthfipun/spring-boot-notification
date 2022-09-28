9115680b0cb59a369c513b9669fe24182b4a9ad4
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-09-28 13:10:44 +0700

    init

---
+++ src/test/kotlin/github/luthfipun/springbootnotification/SpringBootNotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.luthfipun.springbootnotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootNotificationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
